569a0259d4a116e7b99d986a45e10a9c547f5218
mihan2007 <mihan2007@yandex.ru>
2026-01-19 14:43:08 +0300

    Initial commit

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = "sr-cards-v1";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./words.json",
+        "./manifest.json"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
